aa4c2c83d6001232acb9a1765e342fdd0c3a2d2b
Igor <jurkiewiczigor19@gmail.com>
2024-04-05 14:18:16 +0200

    Delete Website with Spring directory

--- Website with Spring/target/classes/db.migration/V1__tabela_danych.sql
+++
@@ -1,7 +0,0 @@
-drop table if exists Dane;
-create table Dane(
-        ID        int primary key auto_increment,
-        nazwa varchar(100) not null,
-        kontakt varchar(100) not null,
-        wiadomosc varchar(300) not null,
-)
